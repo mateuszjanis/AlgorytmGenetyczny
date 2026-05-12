eb621c412eba336cd7af56216c5301642cd821a8
Mateusz Janis <mateusz.janis.04@gmail.com>
2026-05-12 14:04:43 +0200

    TParam class

---
+++ AlgorytmGenetyczny/TParam.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+
+#include "TParam.h"
+
+using namespace std;
+
+TParam::TParam(double x_start, double x_end, double dx)
+{
+	set_range(x_start, x_end, dx);
+	name = "";
+	val_id = 0;
+}
+
+TParam::TParam(double x_start, double x_end, double dx, double val)
+{
+	set_range(x_start, x_end, dx);
+	set_val(val);
+	name = "";
+	
+}
+
+void TParam::set_range(double x_start, double x_end, double dx)
+{
+	this->x_start = x_start;
+	this->x_end = x_end;
+	this->dx = dx;
+}
+
+int TParam::get_val_id(double val)
+{
+	if (val < x_start) return 0;
+
+	else if (val > x_end) return (x_end - x_start) / dx;
+
+	else
+	{
+		double x = x_start;
+		int _id = 0;
+
+		while (fabs(x + _id * dx - val) > dx / 2) _id++;
+
+		return _id;
+	}
+}
+
+void TParam::info()
+{
+	cout << "\n";
+	cout << "=========================\n";
+	cout << "== name: " << name << "\n";
+	cout << "== range: [" << x_start << "; " << x_end << "; " << dx << "]\n";
+	cout << "== range: [" << x_start << "; " << x_end << "; " << dx << "]\n";
+	cout << "=========================\n";
+}
---
+++ AlgorytmGenetyczny/TParam.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+class TParam
+{
+private:
+	std::string name;
+	double x_start, x_end, dx;
+	int val_id;
+
+public:
+	TParam(double x_start, double x_end, double dx);
+	TParam(double x_start, double x_end, double dx, double val);
+
+	void set_name(std::string name) { this->name = name; }
+	std::string get_name() { return name; }
+
+	void set_range(double x_start, double x_end, double dx);
+
+	double set_val(double val) { val_id = get_val_id(val); };
+	double get_val() { return x_start + val_id * dx; };
+
+	void info();
+
+private:
+	int get_val_id(double val);
+
+
+};
+
